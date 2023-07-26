91a74b785b6b478c8fb276193562d87bd8bac80a
Diego Saavedra <dsaavedra88@gmail.com>
2023-07-26 16:20:56 -0500

    Update

--- blog/publicaciones/urls.py
+++ blog/publicaciones/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('lista/', views.lista_publicaciones, name='lista_publicaciones'),
+    path('lista/', views.lista_publicaciones, name='lista_publicaciones'),
     path('crear/', views.crear_publicacion, name='crear_publicacion'),
+    path('actualizar/<int:pk>/', views.actualizar_publicacion, name='actualizar_publicacion'),
 ]
--- blog/publicaciones/views.py
+++ blog/publicaciones/views.py
@@ -1,6 +1,7 @@
 # views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PublicacionForm
+from .models import Publicacion
 
 def crear_publicacion(request):
     if request.method == 'POST':
@@ -11,3 +12,19 @@ def crear_publicacion(request):
     else:
         form = PublicacionForm()
     return render(request, 'crear_publicacion.html', {'form': form})
+
+def lista_publicaciones(request):
+    publicaciones = Publicacion.objects.all()
+    return render(request, 'lista_publicaciones.html', {'publicaciones': publicaciones})
+
+def actualizar_publicacion(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    if request.method == 'POST':
+        form = PublicacionForm(request.POST, instance=publicacion)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_publicaciones')
+    else:
+        form = PublicacionForm(instance=publicacion)
+    return render(request, 'actualizar_publicacion.html', {'form': form})
+
