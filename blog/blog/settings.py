53040214e666ec479d648f34aed2bf6e4d9cfb67
Diego Saavedra <dsaavedra88@gmail.com>
2023-07-31 14:17:29 -0500

    Update API Rest Framework

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'publicaciones',
+    'drf_yasg',
     'rest_framework',
+    'publicaciones',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
